9af66e651545c5c817abc0c6d6a30a5978cb6fed
tamaravd <63185522+tamaravd@users.noreply.github.com>
2020-05-06 20:04:59 +0200

    Add files via upload
    Guess the number game

---
+++ script.js
@@ -0,0 +1,10 @@
+/* let naam = document.getElementById("naam").value;
+
+submit.onclick = function(){
+    if (name != null){ 
+        document.getElementById("naam").innerHTML=
+        "Hey " + naam;
+    
+    }
+}; */
+
